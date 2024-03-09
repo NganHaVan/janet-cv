38137dac5a241e59dea5ab039ded96e1711fc39d
NganHaVan <vannunganha@gmail.com>
2024-03-09 17:32:34 +0200

    add travel ui to portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,28 +1,29 @@
 import ShoppingImg from "../assets/projects/shopping.jpeg";
 import ChargingImg from "../assets/projects/charging.jpeg";
+import CampsiteImg from "../assets/projects/campsite.png";
 
 export const data = [
   {
     id: 1,
-    name: "Shopping app",
+    name: "Shopping app (Fullstack)",
     image: ShoppingImg,
     github: "https://github.com/NganHaVan/NodeSecondhandShopping",
     live: "https://shopping-app-4rgr.onrender.com/"
   },
   {
     id: 2,
-    name: "Charging Booking system",
+    name: "Charging Booking system (Backend Only)",
     image: ChargingImg,
     github: "https://github.com/NganHaVan/charging_app",
     live: ""
+  },
+  {
+    id: 3,
+    name: "Travel UI App (UI Only)",
+    image: CampsiteImg,
+    github: "https://github.com/NganHaVan/travel-app-ui",
+    live: "https://travel-app-ui.site/"
   }
-  // {
-  //   id: 3,
-  //   name: "React JS Application",
-  //   image: WorkIm,
-  //   github: "",
-  //   live: ""
-  // },
   // {
   //   id: 4,
   //   name: "React JS Application",
